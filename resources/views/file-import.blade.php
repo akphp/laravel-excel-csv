32f3a2d49d415217149e76a2d5ee12b583f59a1a
akphp <akramalsaidi9@gmail.com>
2020-10-25 18:08:18 +0200

    edit 001

--- resources/views/file-import.blade.php
+++ resources/views/file-import.blade.php
@@ -14,6 +14,9 @@
             Laravel 7 Import and Export CSV & Excel to Database Example
         </h2>
 
+
+
+
         <form action="{{ route('file-import') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="form-group mb-4" style="max-width: 500px; margin: 0 auto;">
@@ -22,6 +25,9 @@
                     <label class="custom-file-label" for="customFile">Choose file</label>
                 </div>
             </div>
+
+
+            
             <button class="btn btn-primary">Import data</button>
             <a class="btn btn-success" href="{{ route('file-export') }}">Export data</a>
         </form>
